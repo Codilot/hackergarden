8e336fff0eaa6b91fa54c90c34d9f8fb9403ae3f
codilot <30231189+Codilot@users.noreply.github.com>
2020-04-28 14:37:38 +0200

    add instagram post include with fetch script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,23 @@
 window.onload = function () {
   document.body.className += "loaded";
 };
+
+function createImages(postId, node) {
+  fetch(`https://instagram.com/p/${postId}/media/?size=m`)
+    .then((response) => {
+      return response.url;
+    })
+    .then((url) => {
+      let image = new Image();
+      image.src = url;
+      node.appendChild(image);
+    });
+}
+
+const $element = document.querySelector(".hackergarden__instagram");
+let instaNodes = $element.querySelectorAll("div[data-id]");
+if (instaNodes) {
+  instaNodes.forEach((instaNode) => {
+    createImages(instaNode.dataset.id, instaNode);
+  });
+}
