3004b95bd7ef3d1c3bd47613bdb90e5c6fbbd8d0
codilot <30231189+Codilot@users.noreply.github.com>
2020-11-25 08:38:42 +0100

    Remove instagram posts as the api has changed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,25 +1,25 @@
-function createImagesFromInstagram(postId, node) {
-  fetch(`https://instagram.com/p/${postId}/media/?size=m`)
-    .then((response) => {
-      return response.url;
-    })
-    .then((url) => {
-      let placeholder = node.querySelector(
-        ".hackergarden__loading__placeholder"
-      );
-      node.removeChild(placeholder);
-      let image = new Image();
-      image.src = url;
-      node.appendChild(image);
-    });
-}
+// function createImagesFromInstagram(postId, node) {
+//   fetch(`https://instagram.com/p/${postId}/media/?size=m`)
+//     .then((response) => {
+//       return response.url;
+//     })
+//     .then((url) => {
+//       let placeholder = node.querySelector(
+//         ".hackergarden__loading__placeholder"
+//       );
+//       node.removeChild(placeholder);
+//       let image = new Image();
+//       image.src = url;
+//       node.appendChild(image);
+//     });
+// }
 
-const $element = document.querySelector(".hackergarden__instagram");
-if ($element) {
-  let instaNodes = $element.querySelectorAll("div[data-id]");
-  if (instaNodes) {
-    instaNodes.forEach((instaNode) => {
-      createImagesFromInstagram(instaNode.dataset.id, instaNode);
-    });
-  }
-}
+// const $element = document.querySelector(".hackergarden__instagram");
+// if ($element) {
+//   let instaNodes = $element.querySelectorAll("div[data-id]");
+//   if (instaNodes) {
+//     instaNodes.forEach((instaNode) => {
+//       createImagesFromInstagram(instaNode.dataset.id, instaNode);
+//     });
+//   }
+// }
